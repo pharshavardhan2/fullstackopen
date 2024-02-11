ecba6aaba272f92399a9492481a83298f8087381
Harsha <pharshavardhan2@outlook.com>
2024-02-11 23:23:23 +0530

    solved step4 of unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,6 +9,8 @@ const Statistics = ({ good, neutral, bad }) => {
   const average = (good  - bad) / all;
   const positive = good / all * 100;
 
+  if (all === 0) return(<div>No feedback given</div>)
+
   return (
     <>
       <h1>statistics</h1>
