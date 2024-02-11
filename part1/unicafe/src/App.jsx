db04ffd65b9b9ad6bd75d08514b3a59b1282b965
Harsha <pharshavardhan2@outlook.com>
2024-02-11 23:11:01 +0530

    solved step1 of unicafe

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
+const Metric = ({ text, value }) => <p>{text} {value}</p>
+
+const DisplayFeedback = ({ good, neutral, bad }) => {
+  return (
+    <>
+      <h1>statistics</h1>
+      <Metric text='good' value={good} />
+      <Metric text='neutral' value={neutral} />
+      <Metric text='bad' value={bad} />
+    </> 
+  );
+}
+
+function App() {
+  const [ good, setGood ] = useState(0);
+  const [ neutral, setNeutral ] = useState(0);
+  const [ bad, setBad ] = useState(0);
+
+  const handleClick = (setFunction, value) => () => setFunction(value); 
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={handleClick(setGood, good + 1)} text='good' />
+      <Button onClick={handleClick(setNeutral, neutral + 1)} text='neutral' />
+      <Button onClick={handleClick(setBad, bad + 1)} text='bad' />
+      <DisplayFeedback good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App
