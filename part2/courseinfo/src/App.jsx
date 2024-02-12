bcbe9f62c2768eb4c8ee50ff56f8a454eb771858
Harsha <pharshavardhan2@outlook.com>
2024-02-12 14:17:08 +0530

    solved step6 of part2/courseinfo

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,59 @@
+const Header = ({ text }) => {
+  return (
+    <h1>{text}</h1>
+  )
+}
+
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map(part => <Part key={part.id} part={part} />)}
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header text={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Render Arrays',
+        exercises: 6,
+        id: 4
+      }
+    ]
+  }
+
+  return (
+    <Course course={course} />
+  )
+}
+
+export default App
