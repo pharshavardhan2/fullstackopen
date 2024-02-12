78856aaa154931c22ead298654e9d7736ae27723
Harsha <pharshavardhan2@outlook.com>
2024-02-12 19:55:53 +0530

    solved step4 of part2/phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,31 +1,43 @@
 import { useState } from 'react'
 
+const Contact = (props) => <p>{props.contact.name} {props.contact.number}</p>
+
+const Contacts = ({ contacts, queryName }) => {
+  const searchContacts = (query) => contacts.filter(contact => contact.name.toLowerCase().includes(query.toLowerCase()))
+  const mapComponent = (contacts) => contacts.map(contact => <Contact key={contact.name} contact={contact} />)
+
+  if (queryName === '') return (<div>{mapComponent(contacts)}</div>)
+  return <div>{mapComponent(searchContacts(queryName))}</div>
+}
+
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    {
-      name: 'Harsha Vardhan',
-      number: '9467873958',
-    },
+  const [ contacts, setContacts ] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
-
-  const handleChangeName = (event) => {
-    setNewName(event.target.value)
+  const [ queryName, setQueryName ] = useState('')
+  
+  const handleChangeQuery = (event) => {
+    const query = event.target.value.trim()
+    setQueryName(query)
   }
 
-  const handleChangeNumber = (event) => {
-    setNewNumber(event.target.value)
-  }
+  const handleChangeName = (event) => setNewName(event.target.value)
+
+  const handleChangeNumber = (event) => setNewNumber(event.target.value)
 
   const handleSubmitContact = (event) => {
     event.preventDefault()
-    if (persons.findIndex(person => person.name === newName) === -1) {
-      const person = {
+    if (contacts.findIndex(contact => contact.name === newName) === -1) {
+      const contact = {
         name: newName,
         number: newNumber,
       }
-      setPersons(persons.concat(person))
+      setContacts(contacts.concat(contact))
       setNewName('')
       setNewNumber('')
     } else {
@@ -36,6 +48,12 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        <label>
+          filter shown with <input value={queryName} onChange={handleChangeQuery}/>
+        </label>
+      </div>
+      <h2>Add New Contact</h2>
       <form onSubmit={handleSubmitContact}>
         <div>
           <label>
@@ -52,9 +70,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>
-        {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
-      </div>
+      <Contacts contacts={contacts} queryName={queryName} />
     </div>
   )
 }
