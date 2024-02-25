4ee835b8642b231a09cfdb3469c8919f98900055
Harsha <pharshavardhan2@outlook.com>
2024-02-25 13:44:52 +0530

    added validation errors

--- part2/phonebook/src/components/ContactForm.jsx
+++ part2/phonebook/src/components/ContactForm.jsx
@@ -9,6 +9,11 @@ const ContactForm = ({ contacts, setContacts, setNotificationMessage }) => {
 
   const handleChangeNumber = (event) => setNewNumber(event.target.value)
 
+  const flashNotification = (content, className, time) => {
+    setNotificationMessage({ content, className })
+    setTimeout(() => setNotificationMessage(null), time)
+  }
+
   const handleSubmitContact = (event) => {
     event.preventDefault()
     const existingContact = contacts.find(contact => contact.name === newName)
@@ -20,29 +25,20 @@ const ContactForm = ({ contacts, setContacts, setNotificationMessage }) => {
       contactService.create(contact)
         .then(returnedContact => {
           setContacts(contacts.concat(returnedContact))
-          setNotificationMessage({
-            content: `Added ${contact.name}`,
-            className: 'success'
-          })
-          setTimeout(() => setNotificationMessage(null), 5000)
+          flashNotification(`Added ${contact.name}`, 'success', 5000)
+        })
+        .catch(error => {
+          flashNotification(error.response.data.error, 'error', 5000)
         })
     } else {
       if (window.confirm(`${existingContact.name} is already added to phonebook, replace the old number with a new one?`)) {
         contactService.update(existingContact.id, { ...existingContact, number: newNumber })
           .then(returnedContact => {
             setContacts(contacts.map(contact => contact.id !== existingContact.id ? contact : returnedContact))
-            setNotificationMessage({
-              content: `Updated ${existingContact.name}'s number`,
-              className: 'success'
-            })
-            setTimeout(() => setNotificationMessage(null), 5000)
+            flashNotification(`Updated ${existingContact.name}'s number`, 'success', 5000)
           })
           .catch(error => {
-            setNotificationMessage({
-              content: `Information of ${existingContact.name} has already been remove from server`,
-              className: 'error'
-            })
-            setTimeout(() => setNotificationMessage(null), 5000)
+            flashNotification(error.response.data.error, 'error', 5000)
           })
       }
     }
